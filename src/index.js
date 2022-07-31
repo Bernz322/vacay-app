ab18936d0988f38feb485e0d5f8b00976f3a1a81
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-07-31 18:57:13 +0800

    add contactSlice in store

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ import listingReducer from './features/listing/listingSlice'
 import reservationReducer from './features/reservation/reservationSlice'
 import reviewReducer from './features/review/reviewSlice'
 import userReducer from './features/user/userSlice'
+import contactReducer from './features/contact/contactSlice'
 
 import App from './App';
 import "./index.css"
 
 const store = configureStore({
-  reducer: { auth: authReducer, listing: listingReducer, reservation: reservationReducer, review: reviewReducer, user: userReducer }
+  reducer: { auth: authReducer, listing: listingReducer, reservation: reservationReducer, review: reviewReducer, user: userReducer, contact: contactReducer }
 })
 
 ReactDOM.render(
