9a451b29e1db281e82a93cf6096c963fc1dd933f
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-08-04 19:01:23 +0800

    remove Link when a user who left a review deleted themselves

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -28,17 +28,30 @@ export default function ReviewCard({ reservation, review }) {
         <Grid.Col xs={6} lg={4} className={classes.review} >
             <Container className={classes.card}>
                 <Group position='apart'>
-                    <Link to={`/profile/${reservation?.User?.id}`}>
+                    {reservation?.User?.id ?
+                        <Link to={`/profile/${reservation?.User?.id}`}>
+                            <Group position='left'>
+                                <Avatar src={reservation?.User?.profile_image} alt={reservation?.User?.name} radius="xl" />
+                                <div>
+                                    <Text size="sm">{reservation?.User?.name}</Text>
+                                    <Text size="xs" color="dimmed">
+                                        {dayjs(reservation?.createdAt).fromNow()}
+                                    </Text>
+                                </div>
+                            </Group>
+                        </Link>
+                        :
                         <Group position='left'>
                             <Avatar src={reservation?.User?.profile_image} alt={reservation?.User?.name} radius="xl" />
                             <div>
-                                <Text size="sm">{reservation?.User?.name}</Text>
+                                <Text size="sm">Deleted User</Text>
                                 <Text size="xs" color="dimmed">
                                     {dayjs(reservation?.createdAt).fromNow()}
                                 </Text>
                             </div>
                         </Group>
-                    </Link>
+                    }
+
                     <Text size="sm"> Rated: {reservation?.Review?.rating || review?.rating} ⭐</Text>
                 </Group>
                 <Text className={classes.body} size="sm">
